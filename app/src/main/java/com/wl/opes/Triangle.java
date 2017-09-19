7bf34a66ff4f7cf76ecf16ecc22e1967d173761a
Fly <fly@FlydeMacBook-Pro.local>
2017-09-19 20:36:52 +0800

    注释

--- app/src/main/java/com/wl/opes/Triangle.java
+++ app/src/main/java/com/wl/opes/Triangle.java
@@ -12,7 +12,7 @@ import java.nio.FloatBuffer;
 
 public class Triangle {
 
-    FloatBuffer vetextBuff;
+    FloatBuffer vetextBuffer;
 
     private String vetexShader =
             "attribute vec4 vPosition;\n" +
@@ -54,9 +54,9 @@ public class Triangle {
         ByteBuffer byteBuffer = ByteBuffer.allocateDirect(triangleCoords.length*4);
         byteBuffer.order(ByteOrder.nativeOrder());
 
-        vetextBuff = byteBuffer.asFloatBuffer();
-        vetextBuff.put(triangleCoords);
-        vetextBuff.position(0);
+        vetextBuffer = byteBuffer.asFloatBuffer();
+        vetextBuffer.put(triangleCoords);
+        vetextBuffer.position(0);
 
         //加载 顶点着色器 和 片段着色器 并 添加到 GLES中
         int vertexShader = MyGLRenderer.loadShader(GLES20.GL_VERTEX_SHADER,vetexShader);
@@ -78,16 +78,16 @@ public class Triangle {
 
         //获取变换矩阵 句柄，设置变换矩阵的值
         mMVPMatrixHandle = GLES20.glGetUniformLocation(program,"vMatrix");
-        GLES20.glUniformMatrix4fv(mMVPMatrixHandle,1,false,mvpMatrix,0);
+        GLES20.glUniformMatrix4fv(mMVPMatrixHandle,1,false,mvpMatrix,0);//location count transpose(转置) value offset
 
         //获取 顶点着色器 句柄，设置 顶点数据
         positionHandle = GLES20.glGetAttribLocation(program,"vPosition");
         GLES20.glEnableVertexAttribArray(positionHandle);
-        GLES20.glVertexAttribPointer(positionHandle,COORDS_PER_VERTEX,GLES20.GL_FLOAT,false,vertexStride,vetextBuff);
+        GLES20.glVertexAttribPointer(positionHandle,COORDS_PER_VERTEX,GLES20.GL_FLOAT,false,vertexStride, vetextBuffer);//index size type normalized stride buffer
 
         //获取 片段着色器 句柄， 设置颜色数据
         colorHandle = GLES20.glGetUniformLocation(program,"vColor");
-        GLES20.glUniform4fv(colorHandle,1,color,0);
+        GLES20.glUniform4fv(colorHandle,1,color,0);//location count data offset
 
         //绘制三角形，关闭 顶点着色器 句柄
         GLES20.glDrawArrays(GLES20.GL_TRIANGLES,0,vertexCount);
