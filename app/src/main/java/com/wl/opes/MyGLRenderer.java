6d73ea28dd20a6c1c5784d6c227f24ca8a6fceb0
Fly <fly@FlydeMacBook-Pro.local>
2017-09-19 20:23:30 +0800

    代码重构，加入变换矩阵

--- app/src/main/java/com/wl/opes/MyGLRenderer.java
+++ app/src/main/java/com/wl/opes/MyGLRenderer.java
@@ -2,10 +2,7 @@ package com.wl.opes;
 
 import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
-
-import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
-import java.nio.FloatBuffer;
+import android.opengl.Matrix;
 
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
@@ -16,83 +13,39 @@ import javax.microedition.khronos.opengles.GL10;
 
 public class MyGLRenderer implements GLSurfaceView.Renderer{
 
-    private String vetexShader = "attribute vec4 vPosition;\n" +
-            " void main() {\n" +
-            "     gl_Position = vPosition;\n" +
-            " }";
 
-    private String colorShader = "precision mediump float;\n" +
-            " uniform vec4 vColor;\n" +
-            " void main() {\n" +
-            "     gl_FragColor = vColor;\n" +
-            " }";
+   Triangle triangle;
 
-    float triangleCoords[] = {
-            0.5f,  0.5f, 0.0f, // top
-            -0.5f, -0.5f, 0.0f, // bottom left
-            0.5f, -0.5f, 0.0f  // bottom right
-    };
+    private final float[] mMVPMatrix = new float[16];
+    private final float[] mProjectionMatrix = new float[16];
+    private final float[] mViewMatrix = new float[16];
 
-    float color[] = { 0.63671875f, 0.76953125f, 0.22265625f, 1.0f };
-
-
-    FloatBuffer vetextBuff;
-
-    int program;
-
-    int positionHandle;
-
-    int colorHandle;
-
-    //顶点纬度
-    static final int COORDS_PER_VERTEX = 3;
-    //一个顶点 字节长度
-    private final int vertexStride = COORDS_PER_VERTEX * 4;
-    //顶点个数
-    private final int vertexCount = triangleCoords.length / COORDS_PER_VERTEX;
 
     @Override
     public void onSurfaceCreated(GL10 gl10, EGLConfig eglConfig) {
 
         GLES20.glClearColor(255f,255f,255f,1.0f);
 
-        ByteBuffer byteBuffer = ByteBuffer.allocateDirect(triangleCoords.length*4);
-        byteBuffer.order(ByteOrder.nativeOrder());
-
-        vetextBuff = byteBuffer.asFloatBuffer();
-        vetextBuff.put(triangleCoords);
-        vetextBuff.position(0);
-
-        int vertexShader = loadShader(GLES20.GL_VERTEX_SHADER,vetexShader);
-        int fragmentShader = loadShader(GLES20.GL_FRAGMENT_SHADER,colorShader);
-
-        program = GLES20.glCreateProgram();
-
-        GLES20.glAttachShader(program,vertexShader);
-        GLES20.glAttachShader(program,fragmentShader);
-        GLES20.glLinkProgram(program);
+        triangle = new Triangle();
     }
 
     @Override
     public void onSurfaceChanged(GL10 gl10, int width, int height) {
         GLES20.glViewport(0,0,width,height);
+
+        float ratio = (float)width/height;
+
+        Matrix.frustumM(mProjectionMatrix,0,-ratio,ratio,-1,1,3,7);
+
+        Matrix.setLookAtM(mViewMatrix,0,0,0,7.0f,0.0f,0f,0.0f,0.0f,1.0f,0.0f);
+
+        Matrix.multiplyMM(mMVPMatrix,0,mProjectionMatrix,0,mViewMatrix,0);
     }
 
     @Override
     public void onDrawFrame(GL10 gl10) {
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
-
-        GLES20.glUseProgram(program);
-
-        positionHandle = GLES20.glGetAttribLocation(program,"vPosition");
-        GLES20.glEnableVertexAttribArray(positionHandle);
-        GLES20.glVertexAttribPointer(positionHandle,COORDS_PER_VERTEX,GLES20.GL_FLOAT,false,vertexStride,vetextBuff);
-
-        colorHandle = GLES20.glGetUniformLocation(program,"vColor");
-        GLES20.glUniform4fv(colorHandle,1,color,0);
-
-        GLES20.glDrawArrays(GLES20.GL_TRIANGLES,0,vertexCount);
-        GLES20.glDisableVertexAttribArray(positionHandle);
+        triangle.draw(mMVPMatrix);
     }
 
     public static int loadShader(int type,String shaderCode){
---
+++ app/src/main/java/com/wl/opes/Triangle.java
@@ -0,0 +1,97 @@
+package com.wl.opes;
+
+import android.opengl.GLES20;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+
+/**
+ * Created by fly on 2017/9/19.
+ */
+
+public class Triangle {
+
+    FloatBuffer vetextBuff;
+
+    private String vetexShader =
+            "attribute vec4 vPosition;\n" +
+             "uniform mat4 vMatrix;\n"+
+            " void main() {\n" +
+            "     gl_Position = vMatrix*vPosition;\n" +
+            " }";
+
+    private String colorShader = "precision mediump float;\n" +
+            " uniform vec4 vColor;\n" +
+            " void main() {\n" +
+            "     gl_FragColor = vColor;\n" +
+            " }";
+
+    float triangleCoords[] = {
+            0.5f,  0.5f, 0.0f, // top
+            -0.5f, -0.5f, 0.0f, // bottom left
+            0.5f, -0.5f, 0.0f  // bottom right
+    };
+
+    float color[] = { 0.63671875f, 0.76953125f, 0.22265625f, 1.0f };
+
+    int program;
+
+    //顶点纬度
+    static final int COORDS_PER_VERTEX = 3;
+    //一个顶点 字节长度
+    private final int vertexStride = COORDS_PER_VERTEX * 4;
+    //顶点个数
+    private final int vertexCount = triangleCoords.length / COORDS_PER_VERTEX;
+
+    private int positionHandle;
+    private int colorHandle;
+    private int mMVPMatrixHandle;
+
+    public Triangle() {
+
+        //初始化 顶点buffer 并 添加 顶点数据
+        ByteBuffer byteBuffer = ByteBuffer.allocateDirect(triangleCoords.length*4);
+        byteBuffer.order(ByteOrder.nativeOrder());
+
+        vetextBuff = byteBuffer.asFloatBuffer();
+        vetextBuff.put(triangleCoords);
+        vetextBuff.position(0);
+
+        //加载 顶点着色器 和 片段着色器 并 添加到 GLES中
+        int vertexShader = MyGLRenderer.loadShader(GLES20.GL_VERTEX_SHADER,vetexShader);
+        int fragmentShader = MyGLRenderer.loadShader(GLES20.GL_FRAGMENT_SHADER,colorShader);
+
+        program = GLES20.glCreateProgram();
+
+        GLES20.glAttachShader(program,vertexShader);
+        GLES20.glAttachShader(program,fragmentShader);
+
+        //连接program
+        GLES20.glLinkProgram(program);
+    }
+
+    public void draw(float[] mvpMatrix){
+
+        //启用program
+        GLES20.glUseProgram(program);
+
+        //获取变换矩阵 句柄，设置变换矩阵的值
+        mMVPMatrixHandle = GLES20.glGetUniformLocation(program,"vMatrix");
+        GLES20.glUniformMatrix4fv(mMVPMatrixHandle,1,false,mvpMatrix,0);
+
+        //获取 顶点着色器 句柄，设置 顶点数据
+        positionHandle = GLES20.glGetAttribLocation(program,"vPosition");
+        GLES20.glEnableVertexAttribArray(positionHandle);
+        GLES20.glVertexAttribPointer(positionHandle,COORDS_PER_VERTEX,GLES20.GL_FLOAT,false,vertexStride,vetextBuff);
+
+        //获取 片段着色器 句柄， 设置颜色数据
+        colorHandle = GLES20.glGetUniformLocation(program,"vColor");
+        GLES20.glUniform4fv(colorHandle,1,color,0);
+
+        //绘制三角形，关闭 顶点着色器 句柄
+        GLES20.glDrawArrays(GLES20.GL_TRIANGLES,0,vertexCount);
+        GLES20.glDisableVertexAttribArray(positionHandle);
+    }
+
+}
